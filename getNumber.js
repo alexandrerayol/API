18b310dde79a02ae87a3b02fc28f5caa364ddec9
Alexandre Rayol <122651034+alexandrerayol@users.noreply.github.com>
2023-09-29 17:51:04 -0300

    teste

--- getNumber.js
+++ getNumber.js
@@ -1,5 +1,11 @@
 function getNumber() {
     const url = 'api/number.js';
-    const response = fetch(url).then(data => {return data.json()}).then(data => {console.log(data.id)});
+    const div = document.querySelector('#result');
+    const response = fetch(url)
+    .then(data => {return data.json()})
+    .then(data => {
+        console.log(data.id);
+        div.innerText = data.id;
+    });
 }
 getNumber();
